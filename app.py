a9826a9077e1970509819e980cc6cafef58011c8
Lawrence Liu <laurie@mac.mynetworksettings.com>
2025-05-17 21:01:46 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+import base64
+import signal
+import time
+from datetime import datetime
+from pathlib import Path
+from typing import Optional
+from src.hand_signs import HandSigns
+
+import cv2
+import numpy as np
+from fastapi import Response
+from nicegui import Client, app, core, run, ui
+
+black_1px = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjYGBg+A8AAQQBAHAgZQsAAAAASUVORK5CYII='
+placeholder = Response(content=base64.b64decode(black_1px.encode('ascii')), media_type='image/png')
+
+def convert(frame: np.ndarray) -> bytes:
+    _, imencode_image = cv2.imencode('.jpg', frame)
+    return imencode_image.tobytes()
+
+def setup() -> None:
+    hand_signs = HandSigns()
+    video_capture = cv2.VideoCapture(0)
+    last_frame: Optional[np.ndarray] = None
+    preview_bytes: Optional[bytes] = None
+    is_test_started = False
+    is_capturing = True 
+    hands_detected = False
+
+    @app.get('/video/frame')
+    async def grab_video_frame() -> Response:
+        nonlocal last_frame
+        if not video_capture.isOpened() or not is_capturing:
+            return placeholder # do not capture if webcam is paused
+        ret, frame = await run.io_bound(video_capture.read)
+        if not ret or frame is None:
+            return placeholder
+        last_frame = frame.copy()  # <-- Store the last valid frame for later use
+
+        hand_signs.detect_async(frame)
+        if hand_signs.result:
+            frame = hand_signs.draw_landmarks_on_image(frame, hand_signs.result)
+        jpeg = await run.cpu_bound(convert, frame)
+        return Response(content=jpeg, media_type='image/jpeg')
+    
+    async def capture_photo():
+        nonlocal preview_bytes, is_capturing, hands_detected
+        if last_frame is not None and last_frame.size > 1:
+            # pause webcam capture
+            is_capturing = False
+            video_image.classes('hidden')
+
+            # run hand detection
+            hand_signs.detect_async(last_frame)
+            if hand_signs.result.hand_landmarks:
+                hands_detected = True
+                # annotate the captured frame with landmarks
+                annotated_frame = hand_signs.draw_landmarks_on_image(last_frame, hand_signs.result)
+                preview_bytes = await run.cpu_bound(convert, annotated_frame)
+
+                # show the preview image
+                preview_image.set_source(f'data:image/jpeg;base64,{base64.b64encode(preview_bytes).decode()}')
+                label_input.classes(remove='hidden')
+                submit_button.classes(remove='hidden')
+            else:
+                hands_detected = False 
+                ui.notify('No hands detected, please try again.')
+                preview_bytes = await run.cpu_bound(convert, last_frame)
+                preview_image.set_source(f'data:image/jpeg;base64,{base64.b64encode(preview_bytes).decode()}')
+            
+            preview_image.classes(remove='hidden')
+            retry_button.classes(remove='hidden')
+
+            capture_button.classes('hidden')
+            start_stop_button.classes('hidden')
+            reset_button.classes('hidden')
+
+        else:
+            ui.notify('No frame available.')
+
+    async def submit_photo():
+        nonlocal preview_bytes, hands_detected
+        if not preview_bytes or not label_input.value.strip():
+            ui.notify('Missing photo or label')
+            return
+        if not hands_detected:
+            ui.notify('No hand sign detected. Please retry.')
+            return
+        
+        label = label_input.value.strip().replace(" ", "_")
+
+        hand_signs.store_to_db(label)
+        ui.notify(f'Saved {label} sign to db')
+        reset_ui()
+
+    async def reset_db():
+        hand_signs.reset_db()
+
+    def update_submit_state():
+        nonlocal preview_bytes, hands_detected
+        if preview_bytes and label_input.value.strip() and hands_detected:
+            submit_button.enable()
+        else:
+            submit_button.disable()
+
+    async def retry_capture():
+        nonlocal hands_detected, is_capturing
+        hands_detected = False  # Reset hands detected flag
+        preview_image.set_source('')  # Clear preview
+        
+        retry_button.classes('hidden')  # Hide retry button
+        label_input.classes('hidden')  # Hide label input and submit button
+        submit_button.classes('hidden')  # Hide submit button
+        reset_button.classes('hidden')
+        
+        video_image.classes(remove='hidden')  # Show webcam feed again
+        capture_button.classes(remove='hidden')  # Show capture button
+        start_stop_button.classes(remove='hidden')
+        
+        # Resume webcam capture
+        is_capturing = True
+
+    # Function to start/stop the test
+    async def start_stop_test():
+        nonlocal is_test_started
+
+        if is_test_started:
+            # Stop the test: show capture button and change the text
+            capture_button.classes(remove='hidden')  # Show capture button
+            reset_button.classes(remove='hidden')
+            start_stop_button.set_text('Start Test')  # Change text to "Start Test"
+            hand_signs.testing = False
+        else:
+            # Start the test: hide capture button and change the text
+            capture_button.classes('hidden')  # Hide capture button
+            reset_button.classes('hidden')
+            start_stop_button.set_text('Stop Test')  # Change text to "Stop Test"
+            hand_signs.testing = True
+
+        # Toggle the test state
+        is_test_started = not is_test_started
+
+    def reset_ui():
+        nonlocal is_capturing, hands_detected
+        video_image.classes(remove='hidden')
+        capture_button.classes(remove='hidden')
+        start_stop_button.classes(remove='hidden')
+        reset_button.classes(remove='hidden')
+        label_input.set_value('')
+        label_input.classes('hidden')
+        submit_button.classes('hidden')
+        preview_image.set_source('')
+        preview_image.classes('hidden')
+        submit_button.disable()
+        retry_button.classes('hidden')
+        is_capturing = True
+
+    with ui.column().classes('justify-center w-full'):
+        with ui.element('div').classes('relative mx-auto max-w-xl w-full aspect-video'):
+            video_image = ui.interactive_image().classes('absolute inset-0 w-full h-full object-cover')
+            ui.timer(interval=0.1, callback=lambda: video_image.set_source(f'/video/frame?{time.time()}'))
+            preview_image = ui.image().classes('absolute inset-0 w-full h-full object-cover hidden')
+
+        with ui.row().classes('justify-center w-full mt-4'):
+            start_stop_button = ui.button('Start Test', on_click=start_stop_test).classes('w-fit mt-2')
+            capture_button = ui.button('Capture Photo', on_click=capture_photo).classes('w-fit mt-2')
+            reset_button = ui.button('Reset DB', on_click=reset_db).classes('w-fit mt-2')
+            label_input = ui.input(placeholder='Enter label for photo', on_change=lambda _: update_submit_state()).classes('mt-2 hidden w-64')
+            submit_button = ui.button('Submit & Save Photo', on_click=submit_photo).classes('hidden mt-2 w-fit')
+            submit_button.disable()
+            retry_button = ui.button('Retry', on_click=retry_capture).classes('hidden mt-2 w-fit')  # Initially hidden
+
+    async def disconnect() -> None:
+        for client_id in Client.instances:
+            await core.sio.disconnect(client_id)
+
+    def handle_sigint(signum, frame) -> None:
+        ui.timer(0.1, disconnect, once=True)
+        ui.timer(1, lambda: signal.default_int_handler(signum, frame), once=True)
+
+    async def cleanup() -> None:
+        await disconnect()
+        video_capture.release()
+        hand_signs.close()
+
+    app.on_shutdown(cleanup)
+    signal.signal(signal.SIGINT, handle_sigint)
+
+app.on_startup(setup)
+ui.run()
---
+++ src/hand_signs.py
@@ -0,0 +1,131 @@
+import cv2
+import mediapipe as mp 
+import numpy as np
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import time 
+from mediapipe.framework.formats import landmark_pb2
+import chromadb
+import uuid
+from PIL import Image, ImageDraw, ImageFont
+
+MARGIN = 10  # pixels
+FONT_SIZE = 10
+FONT_THICKNESS = 4
+HANDEDNESS_TEXT_COLOR = (222,92,48) 
+
+class HandSigns():
+    def __init__(self):
+        self.result = vision.HandLandmarkerResult
+        self.live_landmarker = vision.HandLandmarker
+        self.dbclient = chromadb.PersistentClient()
+        self.collection = self.dbclient.get_or_create_collection(name="my_collection")
+        self.create_live_landmarker()
+        self.testing = False
+
+    def create_live_landmarker(self):
+        def update_result(result: vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+            self.result = result
+
+        options = vision.HandLandmarkerOptions(
+            base_options=python.BaseOptions(model_asset_path='model/hand_landmarker.task'),
+            running_mode=vision.RunningMode.LIVE_STREAM,
+            num_hands=2,
+            result_callback=update_result
+        )
+        self.live_landmarker = self.live_landmarker.create_from_options(options)
+
+    def detect_async(self, frame):
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        self.live_landmarker.detect_async(mp_image, int(time.time() * 1000))
+
+    def reset_db(self):
+        self.dbclient.delete_collection(name="my_collection")
+        self.collection = self.dbclient.get_or_create_collection(name="my_collection")
+
+    def store_to_db(self, label):
+        for i, landmark in enumerate(self.result.hand_landmarks):
+            category = self.result.handedness[i][0].display_name
+            vector = self.coordinates_to_vector(landmark)
+
+            self.collection.add(
+                embeddings = [vector],
+                metadatas = [{"sign": label, "hand": category}],
+                ids = [str(uuid.uuid4())]
+            )
+
+    @staticmethod
+    def coordinates_to_vector(coords):
+        # using the palm base as the reference
+        reference_point = np.array([coords[0].x, coords[0].y, coords[0].z]) 
+
+        x_coords = np.array([coord.x for coord in coords]) - reference_point[0]
+        y_coords = np.array([coord.y for coord in coords]) - reference_point[1]
+        z_coords = np.array([coord.z for coord in coords]) - reference_point[2]
+
+        return np.concatenate([x_coords, y_coords, z_coords])
+
+    def draw_landmarks_on_image(self, rgb_image, detection_result):
+        try:
+            if detection_result.hand_landmarks == []:
+                return rgb_image
+            else:
+                hand_landmarks_list = detection_result.hand_landmarks
+                handedness_list = detection_result.handedness
+                annotated_image = np.copy(rgb_image)
+
+                # Loop through the detected hands to visualize.
+                for idx in range(len(hand_landmarks_list)):
+                    hand_landmarks = hand_landmarks_list[idx]
+
+                    # Draw the hand landmarks.
+                    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+                    hand_landmarks_proto.landmark.extend([
+                        landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) 
+                        for landmark in hand_landmarks
+                    ])
+                    mp.solutions.drawing_utils.draw_landmarks(
+                        annotated_image,
+                        hand_landmarks_proto,
+                        mp.solutions.hands.HAND_CONNECTIONS,
+                        mp.solutions.drawing_styles.get_default_hand_landmarks_style(),
+                        mp.solutions.drawing_styles.get_default_hand_connections_style()
+                    )
+
+                    if self.testing:
+                        handedness = handedness_list[idx]
+                        vector = self.coordinates_to_vector(hand_landmarks)
+
+                        # Get the top left corner of the detected hand's bounding box.
+                        height, width, _ = annotated_image.shape
+                        x_coordinates = [landmark.x for landmark in hand_landmarks]
+                        y_coordinates = [landmark.y for landmark in hand_landmarks]
+                        text_x = int(min(x_coordinates) * width)
+                        text_y = int(min(y_coordinates) * height) - 10  # MARGIN is assumed as 10
+
+                        results = self.collection.query(
+                            query_embeddings=vector,
+                            n_results=1
+                        )
+
+                        if results and results['distances'][0][0] < 0.09 and handedness[0].category_name == results['metadatas'][0][0]['hand']:
+                            sign = results['metadatas'][0][0]['sign']
+                            pil_image = Image.fromarray(annotated_image)
+                            draw = ImageDraw.Draw(pil_image)
+                            font = ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/AppleGothic.ttf", 256)
+                            draw.text((text_x, text_y), sign, font=font, fill=(255, 255, 255))
+                            annotated_image = np.array(pil_image)
+
+                return annotated_image
+        except:
+            return rgb_image
+        
+    def close(self):
+        self.landmarker.close()
+
+
+if __name__ == '__main__':
+    dbclient = chromadb.PersistentClient()
+    collection = dbclient.get_or_create_collection(name="my_collection")
+
+    print(collection.peek())
